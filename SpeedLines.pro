c7f4c0c4d73f4fcbd95e50789953f0efae8e7d25
s4920723 <s4920723@bournemouth.ac.uk>
2019-01-31 00:08:03 +0000

    Added general idea in comments

--- SpeedLines.pro
+++ SpeedLines.pro
@@ -3,6 +3,10 @@ SOURCES += $$PWD/src/*
 OTHER_FILES += $$PWD/examples/Draw2D.cpp \
                 $$PWD/examples/SimpleBlur.cpp \
                 $$PWD/examples/AddInputs.cp \
-                $$PWD/examples/Keymix.cpp
+                $$PWD/examples/Keymix.cpp \
+                $$PWD/examples/Mult.cpp \
+                $$PWD/examples/Rectangle.cpp
+
+
 INCLUDEPATH += /home/s4920723
 LIBS += -L /opt/Nuke11.1v3 -l DDImage
--- src/SpeedLines.cpp
+++ src/SpeedLines.cpp
@@ -5,19 +5,19 @@ static const char* const HELP =
 #include <DDImage/NukeWrapper.h>
 #include <DDImage/Iop.h>
 #include <DDImage/Row.h>
-#include <DDImage/Knobs.h>
 #include <DDImage/DrawIop.h>
 #include <DDImage/Knob.h>
+#include <DDImage/Knobs.h>
+#include <DDImage/DDMath.h>
 
 using namespace DD::Image;
 
 static const char* const interpolation_types[] = {
-  "smooth", "linear", 0
+  "linear", "smooth", 0
 };
 
-class SpeedLines : public Iop
+class SpeedLines : public DrawIop
 {
-   public:
     bool drawX, drawY, drawZ; //Enable/Disable any of the RGB channels from the input
     int keyInterval; //How often a control point gets created
     int lifespan; //How long a control point persists for
@@ -26,7 +26,11 @@ class SpeedLines : public Iop
     enum { LINEAR, SMOOTH };
     int interpolation;
 
-    SpeedLines(Node* node) : Iop(node)
+   public:
+    const char* input_label(int n, char*) const;
+    void _validate(bool);
+    bool draw_engine(int y, int x, int r, float *buffer);
+    SpeedLines(Node* node) : DrawIop(node)
     {
         inputs(2);
         drawX = drawY = drawZ = true;
@@ -36,10 +40,6 @@ class SpeedLines : public Iop
         interpolation = SMOOTH;
     }
 
-    const char* input_label(int n, char*) const;
-    void _validate(bool);
-    void _request(int x, int y, int r, int t, ChannelMask channels, int count);
-    void engine(int y, int x, int r, ChannelMask, Row &);
     virtual void knobs(Knob_Callback);
 
     const char* Class() const { return CLASS; }
@@ -64,22 +64,28 @@ void SpeedLines::_validate(bool for_real)
     set_out_channels(Mask_All);
 }
 
-void SpeedLines::_request(int x, int y, int r, int t, ChannelMask channels, int count)
+bool SpeedLines::draw_engine(int y, int x, int r, float *buffer)
+// y - row, x vertical offset, r - end of row, channels - list of usable channels, row - pointer to the current row being operated on
 {
-    input0().request(x, y, r, t, channels, count);
-}
-
-void SpeedLines::engine(int y, int x, int r, ChannelMask channels, Row& row)
-{
-    row.get(input0(), y, x, r, channels);
-    // Figure out how to get velocity daya -> look into Nuke Architecture
-    // Figure out how to draw pixel -> look into Drawlops
-    // Figure out how to not suck at this -> look into different career paths
+   // row.get(input0(), y, x, r, channels);
+   // Figure out how to get velocity daya -> look into Nuke Architecture
+    // Figure out how to draw pixel -> look into Drawlops/ DDImage/gl.h
     // If it becomes impossible try using OpenSplines
+
+    /* Step 1: Create an array of speed lines
+     * Steo 2: Every speed line has an array of control points and a draw_segment(point A, point B) function.
+     * Step 3: Every control point has XY pixel coordiantes, UV color, XYZ velocity value and Frame
+     * Step 4: On every frame up to the current one go through the buffer and look for UV color matches since they do not change over time
+     * Step 5: Record the averaged out XY pixel coordinates of the UV color matches.
+     * Step 6: Look up velocity values at pixel coordinates.
+     * Step 7: Compare velocity  velocity between this and previuos frame and if (minVel < currentVel-PreviousVel < maxVel) => draw line
+     *  using draw_segments(currentControlPoint.poisiton, previousControlPoint.position)
+     * Steo 8: TBA (lifetime etc.) */
 }
 
 void SpeedLines::knobs(Knob_Callback f)
 {
+    input_knobs(f);
     Bool_knob(f, &drawX, "Draw X");
     Tooltip(f, "Enables/Disables the use of the velocity data on the X axis");
     Bool_knob(f, &drawY, "Draw Y");
@@ -94,12 +100,13 @@ void SpeedLines::knobs(Knob_Callback f)
     Tooltip(f, "The maximal position difference required for a speedline segment to be drawn");
     Enumeration_knob(f, &interpolation, interpolation_types, "interpolation", "Interpolation Type");
     Tooltip(f, "The type of intepolation used to draw each segment of the speedline");
+    output_knobs(f);
 }
 
 
-static Iop* build(Node* node)
+static Op* build(Node* node)
 {
-  return new NukeWrapper(new SpeedLines(node));
+  return new SpeedLines(node);
 }
 
 const Iop::Description SpeedLines::description ( CLASS, "Merge/AddInputs", build);
